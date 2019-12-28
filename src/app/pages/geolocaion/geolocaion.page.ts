d4880f276e7eadf0f09f6da164af01da6cb93c90
Fang <fang.live-@msn.com>
2019-12-28 15:33:26 +0700

    add npm i @angular/core

--- src/app/pages/geolocaion/geolocaion.page.ts
+++ src/app/pages/geolocaion/geolocaion.page.ts
@@ -4,6 +4,7 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { Platform } from '@ionic/angular';
 import { RunService } from '../run/run.service';
 
+import { ModalController} from '@ionic/angular';
 import { Observable } from 'rxjs';
 @Component({
   selector: 'app-geolocaion',
@@ -17,58 +18,74 @@ export class GeolocaionPage implements OnInit {
   lat2:any =14.615755;
   lng2:any =100.893776;
   dis:any;
+  intervalVar: any;
   constructor(
     private geolocation: Geolocation,
     public platform: Platform,
     public runService: RunService,
+    public modalCtrl:ModalController,
     ) {
-    this.initializeApp();
+    // this.initializeApp();
    }
 
   ngOnInit() {
   }
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.getLoc();
-    });
-  }
+  // initializeApp() {
+  //   this.platform.ready().then(() => {
+  //     this.getLoc();
+  //   });
+  // }
   getLoc(){
+    this.intervalVar = setInterval(function () {
+      this.geolocation.getCurrentPosition(
+        {maximumAge: 1000, timeout: 5000,
+         enableHighAccuracy: true }
+        ).then((resp) => {
+              // resp.coords.latitude
+              // resp.coords.longitude
+              //alert("r succ"+resp.coords.latitude)
+
+              // console.log(JSON.stringify( resp.coords));
+              console.log(resp);
+              // alert(JSON.stringify( resp.coords));
+
+              this.lat1=resp.coords.latitude
+              this.lng1=resp.coords.longitude
+              },er=>{
+                //alert("error getting location")
+                alert('Can not retrieve Location')
+              }).catch((error) => {
+              //alert('Error getting location'+JSON.stringify(error));
+              alert('Error getting location - '+JSON.stringify(error))
+              });
+    }.bind(this), 5000);
+
     
-    this.geolocation.getCurrentPosition(
-      {maximumAge: 1000, timeout: 5000,
-       enableHighAccuracy: true }
-      ).then((resp) => {
-            // resp.coords.latitude
-            // resp.coords.longitude
-            //alert("r succ"+resp.coords.latitude)
-            alert(JSON.stringify( resp.coords));
-      
-            this.lat1=resp.coords.latitude
-            this.lng1=resp.coords.longitude
-            },er=>{
-              //alert("error getting location")
-              alert('Can not retrieve Location')
-            }).catch((error) => {
-            //alert('Error getting location'+JSON.stringify(error));
-            alert('Error getting location - '+JSON.stringify(error))
-            });
              
   }
 
-  // calculateDistance(){
+  calculateDistance(){
+    let lati1:any =14.548888;
+    let  lngi1:any =100.907579;
+    let lati2:any =14.5651728;
+    let  lngi2:any =100.8835939;
+    let  dis:any ;
 
-  //     let p = 0.017453292519943295;    // Math.PI / 180
-  //     let c = Math.cos;
-  //     let a = 0.5 - c((this.lat1-this.lat2) * p) / 2 + c(this.lat2 * p) *c((this.lat1) * p) * (1 - c(((this.lng1- this.lng2) * p))) / 2;
-  //     this.dis = (12742 * Math.asin(Math.sqrt(a))); // 2 * R; R = 6371 km
-  //     console.log(this.lat1);
-  //     console.log(this.lat2);
-  //     console.log(this.lng1);
-  //     console.log(this.lng2);
-  //     console.log(this.dis);
-  //     return this.dis;
+      let p = 0.017453292519943295;    // Math.PI / 180
+      let c = Math.cos;
+      let a = 0.5 - c((lati1-lati2) * p) / 2 + c(lati2 * p) *c((lati1) * p) * (1 - c(((lngi1- lngi2) * p))) / 2;
+      dis = (12742 * Math.asin(Math.sqrt(a))); // 2 * R; R = 6371 km
+      console.log(lati1);
+      console.log(lati2);
+      console.log(lngi1);
+      console.log(lngi2);
+      console.log(dis);
+
+      return dis;
     
-  // }
-  
+  }
+  onGetmaplocation(){
+    this.modalCtrl.create({component: MapModalComponent})
+  }
 
 }
---
+++ src/app/shared/share.module.ts
@@ -0,0 +1,9 @@
+import { NgModel } from '@angular/forms';
+
+import { LocationPickerComponent } from './location-picker/location-picker.component';
+import { MapModalComponent } from './map-modal/map-modal.component';
+@NgModel({
+    declarations: [LocationPickerComponent ,MapModalComponent],
+    // imports: [CommonModule, IonicModule],
+})
+export class ShareModule {}
