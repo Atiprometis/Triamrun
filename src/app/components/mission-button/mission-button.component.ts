c172d59f6f48103cbe3113109851962d91dbac1b
Fang <fang.live-@msn.com>
2020-01-19 16:41:06 +0700

    add misstion click

--- src/app/components/mission-button/mission-button.component.ts
+++ src/app/components/mission-button/mission-button.component.ts
@@ -15,6 +15,12 @@ export class MissionButtonComponent implements OnInit {
  
   isLoading = false;
 
+  onClasschangeClick = {};
+  onClasschangeClick2 = {};
+  onClassfade ={}
+  clcikCount = 0;
+  isShow:boolean = false;
+
   constructor(
     private missionService:MissionService,
 
@@ -25,7 +31,7 @@ export class MissionButtonComponent implements OnInit {
   
 
   ngOnInit() {
-    this.missionService.showLoader();
+    // this.missionService.showLoader();
     this.missionService.getMissioninfo()
 
     .subscribe(resp => {
@@ -33,12 +39,39 @@ export class MissionButtonComponent implements OnInit {
       this.missioninfo = resp;
 
       console.log("resp data mission conponent :", this.missioninfo);
-      this.missionService.hideLoader();
+      // this.missionService.hideLoader();
     }),
     error => {
       console.log(error);
 
     };
   }
+
+  showdata(){
+    console.log('show data ',this.missioninfo);
+  }
+
+  setClasschange(){
+    if(this.clcikCount == 2){
+      this.clcikCount =0;
+    }
+    this.clcikCount++;
+    this.onClasschangeClick = {
+      'contaner-mission-botton2':this.clcikCount==1,
+      'contaner-mission-botton':this.clcikCount==2,
+    }
+    
+    this.onClassfade = {
+      'fade':this.clcikCount==1,
+      '':this.clcikCount==2,
+    }
+    if(this.isShow == true){
+      this.isShow = false;
+    }else{
+      this.isShow = true;
+    }
+
+  }
+  
 
 }
