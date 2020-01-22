3e7613bcc14b925b95f7aded093a3e6401b08b43
Fang <fang.live-@msn.com>
2020-01-23 00:06:00 +0700

    add router login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { LoginService } from '../../service/login.service';
 import { Observable } from 'rxjs';
-
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +17,7 @@ export class LoginPage implements OnInit {
 
   constructor(
     public loginService:LoginService,
-
+    private router:Router,
   ) { }
 
   ngOnInit() {
@@ -27,18 +27,24 @@ export class LoginPage implements OnInit {
 
   login(){
     console.log('logindata');
-    console.log(this.logindata);
     
+    // let logindata = {
+    //   u_email: this.u_email,
+    //   u_password: this.u_password,
+    // };
 
+    console.log(this.logindata);
   
     //  let datalogin = JSON.stringify({
     //   'email': this.u_email,
     //   'password': this.u_password,
     //  });
 
-     let data:Observable<any> = this.loginService.getUserlogin(this.logindata);
-     data.map(res => res.json())
-     data.subscribe(data =>{
+     this.loginService.getUserlogin(this.logindata)
+    //  data.map(res => res.json())
+     .subscribe(data =>{
+       
+      //  this.router.navigate(['/mission']);
       console.log('postDataURL ss',data);
      });
   }
